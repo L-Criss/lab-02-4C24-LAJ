d30268276f23045fdd766ef2f9400991a100d98b
lino-avega-joachin <mael.lcja@gmail.com>
2026-05-10 14:18:58 -0500

    2da modificación por 2do colaborador

--- src/StudentModel.java
+++ src/StudentModel.java
@@ -1,6 +1,8 @@
 public class StudentModel {
 
     public void Model(){
-    System.out.println("Modificando por colaborador 2");}
+    System.out.println("Modificando por colaborador 2");
+System.out.println("Modificando por colaborador 2");
+}
     
 }
