b675e786b01404574b51e91375c522e2995479f2
L-Criss <linoc.joachin@gmail.com>
2026-05-10 14:15:46 -0500

    3ra modificación por Responsable

--- src/StudentController.java
+++ src/StudentController.java
@@ -5,6 +5,7 @@ public class StudentController {
     System.out.println("Hola Controller");
 
     System.out.println("Se ha realizado una segunda modificación por el Responsable");
+    System.out.println("Se ha realizado una 3ra modificación por el Responsable");
 
     }
 }
